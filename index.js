832f60409c96ac43640732b62844565570dbf27f
shahid <shahidmuffakir@gmail.com>
2025-05-02 01:43:27 +0530

    create test endpoint

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.get('/test', (req, res) => {
+  res.send('Hello, World!');
+})
 
 app.get('/download', async (req, res) => {
   try {
